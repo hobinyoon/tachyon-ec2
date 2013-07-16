bbbf2bef68dbf1f6af06e7d80523d319f46feed8
Hobin Yoon <hobinyoon@gmail.com>
2013-07-16 00:39:36 -0700

    update file sizes

--- get-sample-data-from-s3.sh
+++ get-sample-data-from-s3.sh
@@ -11,11 +11,5 @@ s3cmd get s3://2012-05-19-sample/hit_data.tsv.10000 /mnt/data
 s3cmd get s3://2012-05-19-sample/hit_data.tsv.10000.gz /mnt/data
 s3cmd get s3://2012-05-19-sample/hit_data.tsv.100000 /mnt/data
 s3cmd get s3://2012-05-19-sample/hit_data.tsv.100000.gz /mnt/data
-s3cmd get s3://2012-05-19-sample/hit_data.tsv.1000000 /mnt/data
-s3cmd get s3://2012-05-19-sample/hit_data.tsv.1000000.gz /mnt/data
-s3cmd get s3://2012-05-19-sample/hit_data.tsv.2000000 /mnt/data
-s3cmd get s3://2012-05-19-sample/hit_data.tsv.2000000.gz /mnt/data
-s3cmd get s3://2012-05-19-sample/hit_data.tsv.3000000 /mnt/data
-s3cmd get s3://2012-05-19-sample/hit_data.tsv.3000000.gz /mnt/data
-s3cmd get s3://2012-05-19-sample/hit_data.tsv.4000000 /mnt/data
-s3cmd get s3://2012-05-19-sample/hit_data.tsv.4000000.gz /mnt/data
+s3cmd get s3://2012-05-19-sample/hit_data.tsv.500000 /mnt/data
+s3cmd get s3://2012-05-19-sample/hit_data.tsv.500000.gz /mnt/data
