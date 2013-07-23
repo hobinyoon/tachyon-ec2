f0fdafa90cf8f00808ca886c17acc6edafaf7dad
Hobin Yoon <hobinyoon@gmail.com>
2013-07-23 20:56:16 +0000

    download to /mnt/tachyon

--- get-sample-data-from-s3.sh
+++ get-sample-data-from-s3.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-sudo mkdir -p /mnt/data
-sudo chown ubuntu /mnt/data
+sudo mkdir -p /mnt/tachyon
+sudo chown ubuntu /mnt/tachyon
 
-s3cmd get s3://2012-05-19-sample/hit_data.tsv.1 /mnt/data
-s3cmd get s3://2012-05-19-sample/hit_data.tsv.100 /mnt/data
-s3cmd get s3://2012-05-19-sample/hit_data.tsv.100.gz /mnt/data
-s3cmd get s3://2012-05-19-sample/hit_data.tsv.1000 /mnt/data
-s3cmd get s3://2012-05-19-sample/hit_data.tsv.1000.gz /mnt/data
-s3cmd get s3://2012-05-19-sample/hit_data.tsv.10000 /mnt/data
-s3cmd get s3://2012-05-19-sample/hit_data.tsv.10000.gz /mnt/data
-s3cmd get s3://2012-05-19-sample/hit_data.tsv.100000 /mnt/data
-s3cmd get s3://2012-05-19-sample/hit_data.tsv.100000.gz /mnt/data
-s3cmd get s3://2012-05-19-sample/hit_data.tsv.500000 /mnt/data
-s3cmd get s3://2012-05-19-sample/hit_data.tsv.500000.gz /mnt/data
+s3cmd get s3://2012-05-19-sample/hit_data.tsv.1 /mnt/tachyon
+s3cmd get s3://2012-05-19-sample/hit_data.tsv.100 /mnt/tachyon
+s3cmd get s3://2012-05-19-sample/hit_data.tsv.100.gz /mnt/tachyon
+s3cmd get s3://2012-05-19-sample/hit_data.tsv.1000 /mnt/tachyon
+s3cmd get s3://2012-05-19-sample/hit_data.tsv.1000.gz /mnt/tachyon
+s3cmd get s3://2012-05-19-sample/hit_data.tsv.10000 /mnt/tachyon
+s3cmd get s3://2012-05-19-sample/hit_data.tsv.10000.gz /mnt/tachyon
+s3cmd get s3://2012-05-19-sample/hit_data.tsv.100000 /mnt/tachyon
+s3cmd get s3://2012-05-19-sample/hit_data.tsv.100000.gz /mnt/tachyon
+s3cmd get s3://2012-05-19-sample/hit_data.tsv.500000 /mnt/tachyon
+s3cmd get s3://2012-05-19-sample/hit_data.tsv.500000.gz /mnt/tachyon
