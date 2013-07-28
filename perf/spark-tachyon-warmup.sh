a14742da3ddca18c69b079ed839890bcf6215a85
Hobin Yoon <hobinyoon@gmail.com>
2013-07-28 21:23:48 +0000

    warm up from tachyon-ec2-0 so that tachyon-ec2-1 can read the files from.

---
+++ perf/spark-tachyon-warmup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+sparkshellcount()
+{
+	local_fn=/mnt/tachyon/$1
+	tachyon_fn=tachyon://tachyon-ec2-0:19998/$1
+
+	echo "spark-shell count"
+
+	jps -l | grep -v Jps | sort -k 2
+	echo
+
+	cd ~/work/spark
+	./spark-shell <<END_SCRIPT
+val s = sc.textFile("$tachyon_fn")
+s.count()
+END_SCRIPT
+}
+
+
+declare -a SIZES=(
+1
+100
+1000
+10000
+100000
+500000
+)
+
+
+run_by_sizes()
+{
+	for s in "${SIZES[@]}"
+	do
+		filename=hit_data.tsv.$s
+		$1 $filename 2>&1
+	done
+}
+
+run_by_sizes sparkshellcount
