5de4c00cf215b41ba7863a69640c9c84b9f8e44b
Hobin Yoon <hobinyoon@gmail.com>
2013-07-30 15:10:04 -0700

    clean up

--- perf/spark-s3n-vs-spark-tachyon-s3n.plot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export INFILE=../result/spark-s3n-vs-spark-tachyon-s3n-07202013-003733
-export OUTFILE=../result/spark-s3n-vs-spark-tachyon-s3n-07202013-003733.pdf
-export TITLE="Spark - S3 vs. Spark - Tachyon - S3"
-gnuplot spark-local-vs-spark-tachyon-local.gnuplot
